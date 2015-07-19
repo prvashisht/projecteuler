6282e3f4e6d9af607d516b5188347c36680dcdfd
Pratyush Vashishat <pratyush.vashishat@gmail.com>
2015-07-19 16:12:49 +0530

    Update 1.cpp
    used an external function to find sum of all the multiples of 3, 5 and 15 in O(1) rather than O(n)

--- 1.cpp
+++ 1.cpp
@@ -1,23 +1,30 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
+
 using namespace std;
+typedef long long ll;
 
+ll n_sum (ll n)
+{
+    return n * (n + 1) / 2;
+}
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, ans = 0;
+int main ()
+{
+    ll test_case = 0;
+    cin >> test_case;
+    while (test_case--) {
+        long long ans = 0, n, t1, t2, t3;
         cin >> n;
-        while (--n) {
-            if (!(n%3) || !(n%5))
-                ans += n;
-        }
-        cout << ans << endl;
+        n--;
+        t1 = t2 = t3 = n;
+        while (t1 % 3)
+            t1--;
+        while (t2 % 5)
+            t2--;
+        while (t3 % 15)
+            t3--;
+        //cout << t1 << " " << t2 << " " << t3 << " " << endl;
+        cout << 3*n_sum(t1/3) + 5*n_sum(t2/5) - 15*n_sum(t3/15) << endl;
     }
     return 0;
 }
